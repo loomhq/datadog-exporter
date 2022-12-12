a583a5f80c72a8ff25b8286039d9156b6f1d4bc3
Sean Lingren <sean.lingren@loom.com>
2022-12-12 15:34:13 -0800

    add monitors command (#3)

--- cmd/datadog.go
+++ cmd/datadog.go
@@ -14,17 +14,23 @@ import (
 
 const fileMode = 0600
 
+// ddc holds the datadog client and other info for all out export operations.
 type ddc struct {
-	dashAPI *datadogV1.DashboardsApi
+	dashAPI    *datadogV1.DashboardsApi
+	monitorAPI *datadogV1.MonitorsApi
 }
 
+// newDDC creates a new datadog client.
 func newDDC() *ddc {
 	dd := datadog.NewAPIClient(datadog.NewConfiguration())
 
-	return &ddc{dashAPI: datadogV1.NewDashboardsApi(dd)}
+	return &ddc{
+		dashAPI:    datadogV1.NewDashboardsApi(dd),
+		monitorAPI: datadogV1.NewMonitorsApi(dd),
+	}
 }
 
-// Dashboards returns a list of all dashboard IDs in the account.
+// dashboards returns a list of all dashboard IDs in the account.
 func (d *ddc) dashboards(ctx context.Context) ([]string, error) {
 	ctx = datadog.NewDefaultContext(ctx)
 
@@ -41,7 +47,24 @@ func (d *ddc) dashboards(ctx context.Context) ([]string, error) {
 	return dbs, nil
 }
 
-// DashboardJSON returns the JSON definition for a given dashboard ID.
+// monitors returns a list of all monitor IDs in the account.
+func (d *ddc) monitors(ctx context.Context) ([]int64, error) {
+	ctx = datadog.NewDefaultContext(ctx)
+
+	list, _, err := d.monitorAPI.ListMonitors(ctx, *datadogV1.NewListMonitorsOptionalParameters())
+	if err != nil {
+		return nil, fmt.Errorf("failed to list monitors: %w", err)
+	}
+
+	mns := []int64{}
+	for _, monitor := range list {
+		mns = append(mns, *monitor.Id)
+	}
+
+	return mns, nil
+}
+
+// dashboardJSON returns the JSON definition for a given dashboard ID.
 func (d *ddc) dashboardJSON(ctx context.Context, id string) ([]byte, error) {
 	ctx = datadog.NewDefaultContext(ctx)
 
@@ -64,6 +87,30 @@ func (d *ddc) dashboardJSON(ctx context.Context, id string) ([]byte, error) {
 	return dash.Bytes(), nil
 }
 
+// monitorJSON returns the JSON definition for a given monitor ID.
+func (d *ddc) monitorJSON(ctx context.Context, id int64) ([]byte, error) {
+	ctx = datadog.NewDefaultContext(ctx)
+
+	_, resp, err := d.monitorAPI.GetMonitor(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get dashboard with id: %d: %w", id, err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body for id: %d: %w", id, err)
+	}
+
+	var dash bytes.Buffer
+	err = json.Indent(&dash, body, "", "    ")
+	if err != nil {
+		return nil, fmt.Errorf("failed to indent json for id: %d: %w", id, err)
+	}
+
+	return dash.Bytes(), nil
+}
+
+// writeJSONToFile writes json bytes to a local file for archiving.
 func (d *ddc) writeJSONToFile(name string, json []byte) error {
 	err := os.WriteFile(name+".json", json, fileMode)
 	if err != nil {
